1b89142c1e0729cff0d8f618dfdc89d6fd0a9048
DzejlanRedzepefendic <dzejlanven@icloud.com>
2021-10-01 19:03:10 +0200

    first commit

---
+++ src/Login.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+let Login = () => {
+  return (
+    <div className='row'>
+      <div className='col-lg-5 col-md-7 mx-auto'>
+        <div className='card border-success shadow-lg my-2'>
+          <div className='card-header border-bottom border'></div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Login
