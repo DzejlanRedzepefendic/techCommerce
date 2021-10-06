38f258ddff3e47d9a4b8e3e0b9a5a95a8b642fce
DzejlanRedzepefendic <dzejlanven@icloud.com>
2021-10-06 16:42:28 +0200

    Routes and package.json changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,24 @@
 import React from 'react'
 import Login from './Login'
+import Register from './Register.jsx'
+import NoMatchPage from './NoMatchPage.jsx'
+import Dashboard from './Dashboard.jsx'
+import { HashRouter } from 'react-router-dom'
+import { Route, Switch } from 'react-router'
 
 const App = () => {
-  return <Login />
+  return (
+    <HashRouter>
+      <div className='container-fluid'>
+        <Switch>
+          <Route path='/' exact={true} component={Login} />
+          <Route path='/register' component={Register} />
+          <Route path='/dashboard' component={Dashboard} />
+          <Route path='*' component={NoMatchPage} />
+        </Switch>
+      </div>
+    </HashRouter>
+  )
 }
 
 export default App
