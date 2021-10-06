0ca6e01592d047af04cd73ee410fdd3f24a4bc88
DzejlanRedzepefendic <dzejlanven@icloud.com>
2021-10-06 17:23:45 +0200

    Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import Login from './Login'
 import Register from './Register.jsx'
 import NoMatchPage from './NoMatchPage.jsx'
 import Dashboard from './Dashboard.jsx'
+import NavBar from './NavBar'
 import { HashRouter } from 'react-router-dom'
 import { Route, Switch } from 'react-router'
 
 const App = () => {
   return (
     <HashRouter>
+      <NavBar />
       <div className='container-fluid'>
         <Switch>
           <Route path='/' exact={true} component={Login} />
